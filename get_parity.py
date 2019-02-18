b99c198723057a11fed7f0016bd27e139936c16e
Alexey Kadyrov <alehak2001@gmail.com>
2019-02-18 19:58:30 +0300

    unit commit

---
+++ get_parity.py
@@ -0,0 +1,8 @@
+a = int(input("Ввведите число: \n"))
+def parity(a):
+    if a%2 :
+        print ("Число нечетно")
+    else :
+        print("Число четно")
+parity(a)
+    
--- my_max.py
+++ my_max.py
@@ -1,4 +1,7 @@
+a = int(input("1 число: \n"))
+b = int(input("2 число: \n"))
 def max(a,b):
     if a>=b:
         return a
     return b
+print("Большее число : ",max(a,b))
